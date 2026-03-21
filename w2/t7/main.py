af69b2cec5217a70cf7ce629b9c12e06a08f5d98
Enni Hirvonen <enniih.hirvonen@gmail.com>
2026-03-21 11:26:08 +0200

    w2t7

--- w2/t7/main.py
+++ w2/t7/main.py
@@ -1,7 +1,9 @@
+import sys
+
 class Main:
     def __init__(self) -> None:
-        
-
+        for line in sys.stdin:
+            print(line.strip()[::-1])
         return None
     
 if __name__ == "__main__":
